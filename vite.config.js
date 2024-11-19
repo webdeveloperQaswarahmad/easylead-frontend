e0a1a3a3052f007a91e929e15748fcfc8f00035a
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2024-11-19 18:34:59 +0500

    netlify configuration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://esaylead-backend-production.up.railway.app",
         changeOrigin:true,
       }
     }
