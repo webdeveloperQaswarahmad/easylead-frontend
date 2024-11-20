56b2dab20bf115886b9039b077c41431ca1df782
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2024-11-20 18:48:29 +0500

    netlify 404

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,9 +8,12 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://esaylead-backend-production.up.railway.app",
-        changeOrigin:true,
-      }
-    }
-  }
-})
+        target: "https://esaylead-backend-production.up.railway.app", // Use env variable for flexibility
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: "dist", // The folder that will be deployed to Netlify
+  },
+});
